cd0a8bc932bb66576ac29bc3127b07ad9af2bb32
Alessandro Power <alessandrojpower@gmail.com>
2015-12-16 09:50:51 -0500

    Improved performance of Problem 23.

--- p023.py
+++ p023.py
@@ -1,5 +1,12 @@
 #!/usr/bin/env python3
 """Project Euler, Problem 23
+
+First, produce a list of all abundant numbers less than 28124; larger abundant
+numbers are not needed as the largest integer that cannot be expressed as the
+sum of two abundant numbers is smaller than this threshold. Next, for every
+positive integer less than 28124 determine whether it appears as a sum of two
+elements in the previous list. If not, add the integer to a running sum of
+integers which cannot be expressed as the sum of two abundant numbers.
 """
 def sum_divisors(n):
     """Return the sum of the proper divisors of n.
@@ -20,38 +27,40 @@ def sum_divisors(n):
 
     return sum_divisors
 
-def binary_search(element, _list):
-    """Return True is 'element' is located in sorted list '_list'.
+def has_sum(n, _list):
+    """Return True if sorted list '_list' has two elements that sum to 'n'.
     """
-    if _list == []:
-        return False
+    # To search through '_list' in linear time, define two indices 'low' and
+    # 'high' which are initially assigned to the first and last indices of
+    # '_list'. Sum the corresponding elements. If the sum is greater than 'n',
+    # decrement 'high'. If the sum is smaller than 'n', increment 'low'. If the
+    # sum equals 'n', cease the search and return True. Finally, is low ever
+    # exceed 'high', then 'n' cannot be expressed as the sum of two elements in
+    # '_list', so cease the search and return False.
+    low, high = 0, len(_list) - 1
+    _sum = _list[low] + _list[high]
 
-    midpoint = len(_list) // 2
-    if _list[midpoint] == element:
-        return True
-    elif _list[midpoint] > element:
-        return binary_search(element, _list[:midpoint])
-    else:
-        return binary_search(element, _list[midpoint+1:])
+    while low < high and _sum != n:
+        if _sum > n:
+            high -= 1
+        else:
+            low += 1
+        _sum = _list[low] + _list[high]
 
+    return _sum == n
 
 def main():
-    """
+    """Print the sum of all positive integers that cannot be expressed as the
+    sum of two abundant numbers.
     """
     # abundant numbers less than 28124
     abundant_nums = [n for n in range(1, 28124) if sum_divisors(n) > n]
 
+    # Loop through all positive integers less than 28124. If an integer cannot
+    # be expressed as the sum of two abundant numbers, add to 'result'.
     result = 0
     for n in range(1, 28124):
-        i = 0
-        sum_found = False
-        while i < len(abundant_nums) and abundant_nums[i] <= n:
-            if binary_search(n - abundant_nums[i], abundant_nums):
-                sum_found = True
-                break
-            i += 1 
-
-        if not sum_found:
+        if not has_sum(n, abundant_nums):
             result += n
 
     print(result)
