18bcdf9dda3e87ff1034ed673395978e96278f13
Alessandro Power <alessandrojpower@gmail.com>
2016-08-04 12:36:11 -0400

    Improved solution to problem 22.

--- 001-050/p022.py
+++ 001-050/p022.py
@@ -5,42 +5,18 @@ FILE_NAME = "p022_names.txt"
 
 def alphabetical_value(name):
     """Return the alphabetical value (ie the sum of the alphabetical positions
-    of each character) of 'name', assuming the 'name' is in capital letters.
+    of each character) of the given name, assuming it is in uppercase letters.
     """
-    value = 0
-    for char in name:
-        value += ord(char) - 64
-
-    return value
+    return sum(ord(char) - ord('A') + 1 for char in name)
 
 
-def total_score():
-    """Return the sum of scores for all names in 'FILE_NAME'.
-    """
-    name_file = open(FILE_NAME, 'r')
-    name_file_text = name_file.read()
-    name_file.close()
+if __name__ == '__main__':
+    with open(FILE_NAME, 'r') as f:
+        file_text = f.read()
 
-    # put 'name_file_text' into an array
-    names = [name[1:-1] for name in name_file_text.split(',')]
-
-    # sort 'names' alphabetically
+    names = [name[1:-1] for name in file_text.split(',')]
     names.sort()
 
-    # compute total score
-    total_score = 0
-    for i in range(len(names)):
-        score = alphabetical_value(names[i]) * (i + 1)
-        total_score += score
-
-    return total_score
-
-
-def main():
-    """Print the sum of scores for all names in 'FILE_NAME'.
-    """
-    print(total_score())
-
-
-if __name__ == "__main__":
-    main()
+    total_score = sum(
+        alphabetical_value(name) * (i + 1) for i, name in enumerate(names))
+    print(total_score)
