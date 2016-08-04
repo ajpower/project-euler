6e604c9628b8caeea17d41f4a76d5e42b5f72cd4
Alessandro Power <alessandrojpower@gmail.com>
2016-08-04 17:40:46 -0400

    Improved solution to problem 29.

--- 001-050/p029.py
+++ 001-050/p029.py
@@ -1,27 +1,13 @@
 #!/usr/bin/env python3
-"""Project Euler, Problem 29
-
-Brute force solution using Python's set. Because sets are implemented
-in Python with a hash table, the solution below will have the same time
-complexity as a solution that finds all integers that are perfect powers and
-removes their duplicate entries.
-"""
+"""Project Euler, Problem 29."""
 A_MIN = 2
 A_MAX = 100
 B_MIN = 2
 B_MAX = 100
 
+if __name__ == '__main__':
+    a_range = range(A_MIN, A_MAX + 1)
+    b_range = range(B_MIN, B_MAX + 1)
 
-def main():
-    """Print the number of distinct powers formed by taking all integer
-    combinations of a^b for A_MIN <= a <= A_MAX and B_MIN <= b <= B_MAX.
-    """
-    a_range = range(A_MIN, A_MAX+1)
-    b_range = range(B_MIN, B_MAX+1)
-
-    distinct_powers = {a**b for a in a_range for b in b_range}
+    distinct_powers = {a ** b for a in a_range for b in b_range}
     print(len(distinct_powers))
-
-
-if __name__ == "__main__":
-    main()
