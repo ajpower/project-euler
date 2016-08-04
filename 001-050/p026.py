208fcfd12302f8732a7322a06ec385e4223a3a48
Alessandro Power <alessandrojpower@gmail.com>
2016-08-04 15:41:13 -0400

    Improved solution to problem 26.

--- 001-050/p026.py
+++ 001-050/p026.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""Project Euler, Problem 26
+"""Project Euler, Problem 26.
 
 To find the cycle length of the fraction 1/d, perform long division but keep
 track of the remainders only. If a remainder is ever equal to zero, there is no
@@ -10,35 +10,21 @@ D_MAX = 1000
 
 
 def cycle_length(d):
-    """Return the cycle length of 1/d.
-    """
-    remainders = [1]
+    """Return the cycle length of 1/d."""
+    remainders = [1]  # 1 / d is 0 remainder 1.
     while True:
-        new_remainder = 10 * remainders[-1] % d
+        remainder = 10 * remainders[-1] % d
 
-        # cycle is found
-        if new_remainder in remainders:
-            return len(remainders) - remainders.index(new_remainder)
-        # no cycle
-        elif new_remainder == 0:
+        # Cycle is found.
+        if remainder in remainders:
+            return len(remainders) - remainders.index(remainder)
+        # No cycle.
+        elif remainder == 0:
             return 0
         else:
-            remainders.append(new_remainder)
-
-def main():
-    """Print the integer below 'D_MAX' such that the inverse of the integer
-    produces the largest cycle.
-    """
-    max_cycle_length = 0
-    d_max = 0
-
-    for d in range(1, D_MAX):
-        if cycle_length(d) > max_cycle_length:
-            max_cycle_length = cycle_length(d)
-            d_max = d
-
-    print(d_max)
+            remainders.append(remainder)
 
 
-if __name__ == "__main__":
-    main()
+if __name__ == '__main__':
+    max_cycle, d_max_cycle = max((cycle_length(d), d) for d in range(1, D_MAX))
+    print(d_max_cycle)
