28fa87cf691b879a0897d5e5251995fb6fd6f8df
Alessandro Power <alessandrojpower@gmail.com>
2016-08-04 13:22:14 -0400

    Improved solution to problem 23.

--- 001-050/p023.py
+++ 001-050/p023.py
@@ -1,70 +1,35 @@
 #!/usr/bin/env python3
-"""Project Euler, Problem 23
+"""Project Euler, Problem 23.
 
-First, produce a list of all abundant numbers less than 28124; larger abundant
-numbers are not needed as the largest integer that cannot be expressed as the
-sum of two abundant numbers is smaller than this threshold. Next, for every
-positive integer less than 28124 determine whether it appears as a sum of two
-elements in the previous list. If not, add the integer to a running sum of
-integers which cannot be expressed as the sum of two abundant numbers.
+Find all abundant numbers less than 28124. Store the sum of every pair of
+abundant numbers in a set and sum all numbers less than 28124 that are not in
+this set.
 """
-def sum_divisors(n):
-    """Return the sum of the proper divisors of n.
-    """
-    # 1 is always a divisor so can be added immediately; including in the loop
-    # adds complications because n cannot be included in the sum of divisors.
-    d = 2
-    sum_divisors = 1
-    while d**2 < n:
-        if n % d == 0:
-            sum_divisors += d + n // d
+from itertools import combinations_with_replacement
 
+LIMIT = 28124
+
+
+def sum_divisors(n):
+    """Return the sum of the proper divisors of n."""
+    divisor_sum = 1  # Include 1 from the start.
+    d = 2
+    while d * d < n:
+        if n % d == 0:
+            divisor_sum += d
+            divisor_sum += n // d
         d += 1
 
-    # special case if n is a perfect square
-    if d**2 == n:
-        sum_divisors += d
+    # Explicit check if n is a perfect square.
+    if d * d == n:
+        divisor_sum += d
 
-    return sum_divisors
-
-def has_sum(n, _list):
-    """Return True if sorted list '_list' has two elements that sum to 'n'.
-    """
-    # To search through '_list' in linear time, define two indices 'low' and
-    # 'high' which are initially assigned to the first and last indices of
-    # '_list'. Sum the corresponding elements. If the sum is greater than 'n',
-    # decrement 'high'. If the sum is smaller than 'n', increment 'low'. If the
-    # sum equals 'n', cease the search and return True. Finally, is low ever
-    # exceed 'high', then 'n' cannot be expressed as the sum of two elements in
-    # '_list', so cease the search and return False.
-    low, high = 0, len(_list) - 1
-    _sum = _list[low] + _list[high]
-
-    while low < high and _sum != n:
-        if _sum > n:
-            high -= 1
-        else:
-            low += 1
-        _sum = _list[low] + _list[high]
-
-    return _sum == n
-
-def main():
-    """Print the sum of all positive integers that cannot be expressed as the
-    sum of two abundant numbers.
-    """
-    # abundant numbers less than 28124
-    abundant_nums = [n for n in range(1, 28124) if sum_divisors(n) > n]
-
-    # Loop through all positive integers less than 28124. If an integer cannot
-    # be expressed as the sum of two abundant numbers, add to 'result'.
-    result = 0
-    for n in range(1, 28124):
-        if not has_sum(n, abundant_nums):
-            result += n
-
-    print(result)
+    return divisor_sum
 
 
-if __name__ == "__main__":
-    main()
+if __name__ == '__main__':
+    abundant_nums = [n for n in range(1, LIMIT) if sum_divisors(n) > n]
+    sum_abundant_pairs = {n + m for n, m in
+                          combinations_with_replacement(abundant_nums, 2)}
+    answer = sum(n for n in range(1, LIMIT) if n not in sum_abundant_pairs)
+    print(answer)
