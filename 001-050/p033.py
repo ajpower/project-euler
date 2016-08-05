902eed2a3b766997d7390f06a28bf4a82d3b012b
Alessandro Power <alessandrojpower@gmail.com>
2016-08-05 12:49:09 -0400

    Improved solution to problem 33.

--- 001-050/p033.py
+++ 001-050/p033.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""Project Euler, Problem 33
+"""Project Euler, Problem 33.
 
 It can be easily seen that the only fractions two digit numerators and
 denominators that allow non-trivial digit cancellation have the form
@@ -12,29 +12,21 @@ With some simple algebra this equation can be rewritten to
 
     9nd + id = 10ni
 
-with n < d. It suffices to check the above equation for all possible cominations
-of i, n, and d such that 1 <= i, n, d <= 9 and n < d.
+with n < d. It suffices to check the above equation for all possible
+combinations of i, n, and d such that 1 <= i, n, d <= 9, and n < d.
 """
-from fractions import gcd
+from itertools import product
+from math import gcd
 
-
-def main():
-    """Print the denominator of the product of all fractions with two digit
-    numerators and denominators that allow non-trivial digit cancellation.
-    """
+if __name__ == '__main__':
     numerator_product = 1
     denominator_product = 1
-    for i in range(1, 10):
-        for d in range(1, 10):
-            for n in range(1, d):
-                if 9*n*d + i*d == 10*n*i:
-                    numerator_product *= n
-                    denominator_product *= d
+    for i, d in product(range(1, 10), repeat=2):
+        for n in range(1, d):
+            if 9 * n * d + i * d == 10 * n * i:
+                numerator_product *= n
+                denominator_product *= d
 
-    final_denominator = denominator_product //
-                        gcd(numerator_product, denominator_product)
+    final_denominator = denominator_product // gcd(numerator_product,
+                                                   denominator_product)
     print(final_denominator)
-
-
-if __name__ == "__main__":
-    main()
