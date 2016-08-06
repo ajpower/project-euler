89f6627925ea0381df5515d52060a426d252cd6d
Alessandro Power <alessandrojpower@gmail.com>
2016-08-06 12:18:44 -0400

    Improved solution to problem 39.

--- 001-050/p039.py
+++ 001-050/p039.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""Project Euler, Problem 39
+"""Project Euler, Problem 39.
 
 From Euclid's formula it can be shown that the perimeter of a right angled
 triangle must be even.
@@ -19,22 +19,14 @@ solutions.
 MAX_PERIMETER = 1000
 
 
-def main():
-    max_solutions, p_max = 0, 0
-    for p in range(2*(MAX_PERIMETER // 4), MAX_PERIMETER + 1, 2):
-        num_solutions = 0
+def num_solutions(p):
+    """Return the number of right angle triangles with integral length sides with
+    the given perimeter."""
+    return sum((p * p - 2 * p * a) % (2 * p - 2 * a) == 0 for a in
+               range(2, p // 2))
 
-        for a in range(2, p // 2 + 1):
-            b = p*(p - 2*a) % (2*(p - a))
-            if b == 0:
-                num_solutions += 1
 
-        if num_solutions > max_solutions:
-            max_solutions = num_solutions
-            p_max = p
-
+if __name__ == '__main__':
+    p_max = max((p for p in range(MAX_PERIMETER // 2, MAX_PERIMETER + 1, 2)),
+                key=num_solutions)
     print(p_max)
-
-
-if __name__ == "__main__":
-    main()
