9d66711aeaca3498aa972babba123f2938739f04
Alessandro Power <alessandrojpower@gmail.com>
2016-08-10 15:04:08 -0400

    Improved solution to problem 45.

--- 001-050/p045.py
+++ 001-050/p045.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""Project Euler, Problem 45
+"""Project Euler, Problem 45.
 
 All odd-indexed triangle numbers -- and only odd-indexed triangle numbers -- are
 hexagonal numbers as well. Hence it is necessary and sufficient to check every
@@ -15,25 +15,21 @@ from math import sqrt
 
 
 def triangle(n):
-    """Return the triangle number with index 'n'.
-    """
+    """Return the triangle number with given index."""
     return n * (n + 1) // 2
 
-def is_pentagonal(x):
-    """Return True if and only if 'x' is a pentagonal number.
-    """
-    return sqrt(24*x + 1).is_integer() and (int(sqrt(24*x + 1)) + 1) % 6 == 0
 
-def main():
+def is_pentagonal(x):
+    """Return True if and only if given number is pentagonal."""
+    return sqrt(24 * x + 1).is_integer() and (sqrt(24 * x + 1) + 1) % 6 == 0
+
+
+if __name__ == '__main__':
     # Start at n = 287, as that is the first odd index greater than the known
     # solution of 285.
     n = 287
     while True:
         if is_pentagonal(triangle(n)):
             print(triangle(n))
-            return
+            break
         n += 2
-
-
-if __name__ == "__main__":
-    main()
