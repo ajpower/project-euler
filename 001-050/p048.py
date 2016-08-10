94ce52bb0fc81b964d9ec2a62133c97305eb0fee
Alessandro Power <alessandrojpower@gmail.com>
2016-08-10 15:24:06 -0400

    Improved solution to problem 48.

--- 001-050/p048.py
+++ 001-050/p048.py
@@ -1,17 +1,14 @@
 #!/usr/bin/env python3
-"""Project Euler, Problem 48
+"""Project Euler, Problem 48.
 
 Simply compute the sum directly, but only keep the last ten digits every
 iteration.
 """
-def main():
+
+if __name__ == '__main__':
     last_10_digits = 0
     for n in range(1, 1001):
-        last_10_digits += n**n
-        last_10_digits = last_10_digits % 10**10
+        last_10_digits += n ** n
+        last_10_digits %= 10 ** 10
 
     print(last_10_digits)
-
-
-if __name__ == "__main__":
-    main()
