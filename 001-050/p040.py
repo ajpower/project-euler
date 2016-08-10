83f6960f21f8c30bb5957b8170f8a4cc499f97c6
Alessandro Power <alessandrojpower@gmail.com>
2016-08-10 12:16:58 -0400

    Improved solution to problem 40.

--- 001-050/p040.py
+++ 001-050/p040.py
@@ -1,42 +1,28 @@
 #!/usr/bin/env python3
-"""Project Euler, Problem 40
+"""Project Euler, Problem 40."""
+from functools import reduce
+from operator import mul
 
-To determine d_n, begin by calculating the number of digits of the number that
-was used to make the nth digit. For example, a 2 digit number (13) was used to
-create the 16th and 17th digits. Next, determine the actual number that was used
-to create the nth digit. Finally, find the appropriate digit.
-"""
+
 def d(n):
-    """Return the nth digit of Champernowe's constant.
-    """
-    # Determine the number of digits of the number that was used to create the
-    # nth digit.
-    num_digits, tmp = 0, n
-    while tmp > 0:
+    """Return the nth digit of Champernowne's constant."""
+    # The largest value of n such that d_n is part of an m digit number is given
+    # by 9*1 + 90*2 + 900*3 + ... + 9*10^(m-1)*(m).
+    num_digits, tmp = 0, 0
+    while tmp < n:
         num_digits += 1
-        tmp -= 9 * (10**(num_digits-1)) * num_digits
+        tmp += 9 * 10 ** (num_digits - 1) * num_digits
 
-    # Determine the number that went into constructing the nth digit.
-    # `digits` tracks the number of actual digits that have been passed in the
-    # for loop.
-    num, digits = 0, 0 
+    num, remaining_digits = 0, n
     for i in range(1, num_digits):
-        num += 9 * (10**(i-1))
-        digits += 9 * (10**(i-1)) * i
-    num += (n - digits - 1) // num_digits + 1
+        num += 9 * 10 ** (i - 1)
+        remaining_digits -= 9 * 10 ** (i - 1) * i
+    num += (remaining_digits - 1) // num_digits + 1
 
-    # Return the appropriate digit of `num`.
-    digit_pos = num_digits - (n - digits - 1) % num_digits
-    return (num // 10**(digit_pos-1)) % 10
+    digit_pos = num_digits - (remaining_digits - 1) % num_digits
+    return num // 10 ** (digit_pos - 1) % 10
 
-def main():
-    # Iterate over all powers of 10 up to 10^6.
-    result = 1
-    for e in range(7):
-        result *= d(10**e)
 
+if __name__ == '__main__':
+    result = reduce(mul, (d(10 ** e) for e in range(7)))
     print(result)
-
-
-if __name__ == "__main__":
-    main()
