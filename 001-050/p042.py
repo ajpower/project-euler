5c3b7adc790dc31ca33c10bdac5baaf3dda08ffb
Alessandro Power <alessandrojpower@gmail.com>
2016-08-10 12:53:33 -0400

    Improved solution to problem 42.

--- 001-050/p042.py
+++ 001-050/p042.py
@@ -1,51 +1,42 @@
 #!/usr/bin/env python3
-"""Project Euler, Problem 42
+"""Project Euler, Problem 42.
 
 A number is triangular if and only if 8*num + 1 is a perfect square.
 """
 FILENAME = "p042_words.txt"
 
+
 def get_words(filename):
-    """Return a list of words in 'filename'.
-    """
-    text = open(filename, 'r').read()
-    
+    """Return a list of words in the given file."""
+    with open(filename, 'r') as f:
+        text = f.read()
+
     # Split by ',', then remove surrounding quotation marks.
     words = [field[1:-1] for field in text.split(',')]
     return words
 
+
 def is_perfect_square(n):
-    """Return True if 'n' is a perfect square.
-    """
+    """Return True if given number is a perfect square."""
     if n < 0:
         return False
 
     x = 0
-    while x**2 <= n:
-        if x**2 == n:
+    while x * x <= n:
+        if x * x == n:
             return True
         x += 1
 
     return False
 
+
 def is_triangular_word(word):
-    """Return True if and only if 'word' is a triangular word.
-    """
-    word_value = 0
-    for char in word:
-        word_value += ord(char) - ord('A') + 1
+    """Return True if and only if given word is a triangular word."""
+    word_value = sum(ord(c) - ord('A') + 1 for c in word)
+    return is_perfect_square(8 * word_value + 1)
 
-    return is_perfect_square(8*word_value + 1)
 
-def main():
-    triangular_words = 0
+if __name__ == '__main__':
     words = get_words(FILENAME)
-    for word in words:
-        if is_triangular_word(word):
-            triangular_words += 1
-
+    triangular_words = sum(is_triangular_word(word) for word in words)
     print(triangular_words)
-
-
-if __name__ == "__main__":
-    main()
