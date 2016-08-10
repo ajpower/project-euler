fdf07eca175ceb0b65c4bdb72524af7f0ce0542e
Alessandro Power <alessandrojpower@gmail.com>
2016-08-10 15:12:32 -0400

    Improved solution to problem 46.

--- 001-050/p046.py
+++ 001-050/p046.py
@@ -1,42 +1,37 @@
 #!/usr/bin/env python3
-"""Project Euler, Problem 46
+"""Project Euler, Problem 46.
 
 For each odd number, check if prime. If so, add to a running list of primes
 (which starts with 2). If not prime, iterate over all primes so far discovered,
 checking if (x - p) / 2 is a perfect square, where x and p are the number and
 prime in question, respectively.
 """
+from itertools import count
 from math import sqrt
 
 
 def is_prime(n):
-    """Return True if and only if 'n' is prime.
-    """
-    if n <= 1:
+    """Return True if and only if given number is prime."""
+    if n < 2:
         return False
-    d = 2
-    while d**2 <= n:
+
+    if n != 2 and n % 2 == 0:
+        return False
+
+    d = 3
+    while d * d <= n:
         if n % d == 0:
             return False
-        d += 1
+        d += 2
     return True
 
-def main():
-    n = 3
-    primes = [2]
-    while True:
+
+if __name__ == '__main__':
+    primes_below = [2]
+    for n in count(start=3, step=2):
         if is_prime(n):
-            primes.append(n)
+            primes_below.append(n)
         else:
-            goldbach_conjecture = False
-            for p in primes:
-                if sqrt((n - p) // 2).is_integer():
-                    goldbach_conjecture = True
-            if not goldbach_conjecture:
+            if not any(sqrt((n - p) // 2).is_integer() for p in primes_below):
                 print(n)
-                return
-
-        n += 2
-
-if __name__ == "__main__":
-    main()
+                break
