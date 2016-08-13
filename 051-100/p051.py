b4ac3ef9f7f92874f9d72aeeeab2e562ab52a773
Alessandro Power <alessandrojpower@gmail.com>
2016-08-13 17:16:59 -0400

    Improved solution to problem 51.

--- 051-100/p051.py
+++ 051-100/p051.py
@@ -6,49 +6,49 @@ a pre-defined limit. Check if the digits 0, 1, or 2 repeat 3 times. If so,
 generate all numbers that are formed by replacing the repeating digits and check
 the number that are prime. If 8 are prime, then the solution has been found.
 """
+import sys
 
 
-def prime_list(n):
-    """Return a list of primes less than or equal to 'n'.
-    """
-    sieve = [True] * (n + 1)
-    sieve[0] = False
-    sieve[1] = False
-    sieve[2] = True
+def primes_below(n):
+    """Return a list of primes below the given number."""
+    sieve = [True] * n
+    sieve[0], sieve[1] = False, False
 
-    i = 3
+    i = 2
     while i ** 2 <= n:
         if sieve[i]:
-            for j in range(i + 2, len(sieve), 2):
-                if sieve[j] and j % i == 0:
-                    sieve[j] = False
-        i += 2
+            for j in range(i * i, n, i):
+                sieve[j] = False
+        i += 1
 
-    return [2] + [p for p in range(3, len(sieve), 2) if sieve[p]]
+    return [p for p, is_prime in enumerate(sieve) if is_prime]
 
 
-def is_prime(n):
-    """Return True if and only if 'n' is prime.
-    """
+def prime(n):
+    """Return True if given number is prime."""
     if n <= 1:
         return False
-    d = 2
+
+    if n != 2 and n % 2 == 0:
+        return False
+
+    d = 3
     while d ** 2 <= n:
         if n % d == 0:
             return False
-        d += 1
+        d += 2
 
     return True
 
 
-def count(n, d):
-    """Return the number of times the digit 'd' appears in the number 'n'.
+def digit_count(digit, num):
+    """Return the number of times the given digit appears in the given number.
     """
-    tmp = n
+    tmp = num
     count = 0
     while tmp > 0:
-        digit = tmp % 10
-        if digit == d:
+        d = tmp % 10
+        if d == digit:
             count += 1
         tmp //= 10
 
@@ -56,10 +56,8 @@ def count(n, d):
 
 
 def eight_prime_value_family(p):
-    """Return True if and only if the prime number 'p' is part of an eight
-    prime value family.
+    """Return True if the prime number is part of an eight prime value family.
     """
-    # Find digits.
     digits = []
     tmp = p
     while tmp > 0:
@@ -68,7 +66,6 @@ def eight_prime_value_family(p):
         tmp //= 10
 
     # Find the repeating digit.
-    repeating_digit = 0
     for d in digits:
         if digits.count(d) == 3:
             repeating_digit = d
@@ -84,25 +81,23 @@ def eight_prime_value_family(p):
         new_num = 0
         for j in range(len(new_digits)):
             new_num += new_digits[j] * 10 ** j
+
         # Check if new_digit is prime and hasn't lost a digit.
-        if new_digits[-1] != 0 and is_prime(new_num):
+        if new_digits[-1] != 0 and prime(new_num):
             count += 1
 
     return count == 8
 
 
-def main():
-    # max_prime defined to be 10**6. If script does not yield solution, more
+if __name__ == '__main__':
+    # max_prime defined to be 10^6. If script does not yield solution, more
     # primes need to be considered.
     max_primes = 10 ** 6
-    primes = prime_list(max_primes)
+    primes = primes_below(max_primes)
     for p in primes:
-        if any([count(p, d) for d in range(3)]):
-            if eight_prime_value_family(p):
-                print(p)
-                return
-    print("Error: max_primes must be larger")
-
-
-if __name__ == "__main__":
-    main()
+        if any([digit_count(digit=d, num=p) for d in range(3)]) and \
+                eight_prime_value_family(p):
+            print(p)
+            break
+    else:
+        print('Error: max_primes must be larger', file=sys.stderr)
